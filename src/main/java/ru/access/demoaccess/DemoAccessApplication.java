e0dea7acbf889a8d6ea2fdb2cf6e14ecde365a1b
alexKv2000 <AlexKv2000@mail.ru>
2024-09-23 20:50:57 +0300

    add db Access and selected all TableA

--- src/main/java/ru/access/demoaccess/DemoAccessApplication.java
+++ src/main/java/ru/access/demoaccess/DemoAccessApplication.java
@@ -21,18 +21,40 @@ public class DemoAccessApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         System.setProperty("hsqldb.method_class_names", "net.ucanaccess.converters.*");
-        template.update("INSERT INTO ttt(NAME, COUNTER, DEPARTMENT) VALUES('Алёна', 24, 2)");
-        List<TableA> tableList = template.query("SELECT ID, NAME, COUNTER, DEPARTMENT FROM ttt;",
-                (rs, rowNum) -> new TableA(rs.getInt("ID"),
-                        rs.getString("NAME"),
-                        rs.getInt("COUNTER"),
-                        rs.getInt("DEPARTMENT")));
+    //    template.update("INSERT INTO ttt(NAME, COUNTER, DEPARTMENT) VALUES('Алёна', 24, 2)");
+        List<TableA> tableList = template.query("SELECT id, DataPlanPay, ID_Project, Data_plan, OPEX, NDSOPEX, CAPEX, NDSCAPEX, Comment, stepProject, duration, interval, Completed FROM planPayProject",
+                (rs, rowNum) -> new TableA(rs.getString("id"),
+                        rs.getString("DataPlanPay"),
+                        rs.getString("ID_Project"),
+                        rs.getString("Data_plan"),
+                        rs.getString("OPEX"),
+                        rs.getString("NDSOPEX"),
+                        rs.getString("CAPEX"),
+                        rs.getString("NDSCAPEX"),
+                        rs.getString("Comment"),
+                        rs.getString("stepProject"),
+                        rs.getString("duration"),
+                        rs.getString("interval"),
+                        rs.getString("Completed")
+                        ));
 
 
         for (TableA table : tableList) {
-            System.out.println("ID: " + table.getId() + ", Name: " + table.getName() + ", Counter: " + table.getCounter() + ", Is Delivery Counter: " + table.isDelivToTwo() + ", Department: " + table.getDepartment());
+            System.out.println("ID: " + table.getId() +
+                    ", DataPlanPay: " + table.getDataPlanPay() +
+                    ", ID_Project: " + table.getID_Project() +
+                    ", Data_plan : " + table.getData_plan() +
+                    ", OPEX : " + table.getOPEX() +
+                    ", OPEX : " + table.getNDSOPEX() +
+                    ", OPEX : " + table.getCAPEX() +
+                    ", OPEX : " + table.getNDSCAPEX() +
+                    ", OPEX : " + table.getComment() +
+                    ", OPEX : " + table.getStepProject() +
+                    ", OPEX : " + table.getDuration() +
+                    ", OPEX : " + table.getInterval() +
+                    ", OPEX : " + table.getCompleted() );
         }
         //template.update("DELETE FROM ttt WHERE NAME = 'Алёна'");
-        System.out.println(tableList.toString());
+        //System.out.println(tableList.toString());
     }
 }
--- src/main/java/ru/access/demoaccess/TableA.java
+++ src/main/java/ru/access/demoaccess/TableA.java
@@ -1,74 +1,153 @@
 package ru.access.demoaccess;
 
 public class TableA {
-    protected int id;
-    protected String Name;
-    protected int counter;
-    protected int department;
+    protected String id;
+    protected String DataPlanPay;
+    protected String ID_Project;
+    protected String Data_plan;
+    protected String OPEX;
+    protected String NDSOPEX;
+    protected String  CAPEX;
+    protected String NDSCAPEX;
+    protected String Comment;
+    protected String stepProject;
+    protected String duration;
+    protected String interval;
+   protected String Completed;
 
-    protected boolean delivToTwo;
-
-    public TableA(int id, String name, int counter, int department) {
+    public TableA(String id, String dataPlanPay, String ID_Project, String data_plan, String OPEX, String NDSOPEX, String CAPEX, String NDSCAPEX, String comment, String stepProject, String duration, String interval, String Completed) {
         this.id = id;
-        this.Name = name;
-        this.counter = counter;
-        this.department = department;
-        this.delivToTwo = getDelivToTwo(counter);
-    }
-
-    public boolean isDelivToTwo() {
-        return this.delivToTwo;
-    }
-
-    public boolean getDelivToTwo(int counter) {
-        return (counter % 2 == 0);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public int getCounter() {
-        return this.counter;
-    }
-
-    public void setCounter(int counter) {
-        this.counter = counter;
-        this.setDelivToTwo(counter);
-    }
-
-    private void setDelivToTwo(int counter) {
-        this.delivToTwo = getDelivToTwo(counter);
-    }
-
-    public int getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(int department) {
-        this.department = department;
+        DataPlanPay = dataPlanPay;
+        this.ID_Project = ID_Project;
+        Data_plan = data_plan;
+        this.OPEX = OPEX;
+        this.NDSOPEX = NDSOPEX;
+        this.CAPEX = CAPEX;
+        this.NDSCAPEX = NDSCAPEX;
+        this.Comment = comment;
+        this.stepProject = stepProject;
+        this.duration = duration;
+        this.interval = interval;
+        this.Completed = Completed;
     }
 
     @Override
     public String toString() {
         return "TableA{" +
-                "id=" + id +
-                ", Name='" + Name + '\'' +
-                ", counter=" + counter +
-                ", department=" + department +
-                ", delivToTwo=" + delivToTwo +
+                "id='" + id + '\'' +
+                ", DataPlanPay='" + DataPlanPay + '\'' +
+                ", ID_Project='" + ID_Project + '\'' +
+                ", Data_plan='" + Data_plan + '\'' +
+                ", OPEX='" + OPEX + '\'' +
+                ", NDSOPEX='" + NDSOPEX + '\'' +
+                ", CAPEX='" + CAPEX + '\'' +
+                ", NDSCAPEX='" + NDSCAPEX + '\'' +
+                ", Comment='" + Comment + '\'' +
+                ", stepProject='" + stepProject + '\'' +
+                ", duration='" + duration + '\'' +
+                ", interval='" + interval + '\'' +
+                ", Completed='" + Completed + '\'' +
                 '}';
     }
+
+    public String getNDSOPEX() {
+        return NDSOPEX;
+    }
+
+    public void setNDSOPEX(String NDSOPEX) {
+        this.NDSOPEX = NDSOPEX;
+    }
+
+    public String getCAPEX() {
+        return CAPEX;
+    }
+
+    public void setCAPEX(String CAPEX) {
+        this.CAPEX = CAPEX;
+    }
+
+    public String getNDSCAPEX() {
+        return NDSCAPEX;
+    }
+
+    public void setNDSCAPEX(String NDSCAPEX) {
+        this.NDSCAPEX = NDSCAPEX;
+    }
+
+    public String getComment() {
+        return Comment;
+    }
+
+    public void setComment(String comment) {
+        Comment = comment;
+    }
+
+    public String getStepProject() {
+        return stepProject;
+    }
+
+    public void setStepProject(String stepProject) {
+        this.stepProject = stepProject;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getInterval() {
+        return interval;
+    }
+
+    public void setInterval(String interval) {
+        this.interval = interval;
+    }
+
+    public String getCompleted() {
+        return Completed;
+    }
+
+    public void setCompleted(String completed) {
+        Completed = completed;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDataPlanPay() {
+        return DataPlanPay;
+    }
+
+    public void setDataPlanPay(String dataPlanPay) {
+        DataPlanPay = dataPlanPay;
+    }
+
+    public String getID_Project() {
+        return ID_Project;
+    }
+
+    public void setID_Project(String ID_Project) {
+        this.ID_Project = ID_Project;
+    }
+
+    public String getData_plan() {
+        return Data_plan;
+    }
+
+    public void setData_plan(String data_plan) {
+        Data_plan = data_plan;
+    }
+
+    public String getOPEX() {
+        return OPEX;
+    }
+
+    public void setOPEX(String OPEX) {
+        this.OPEX = OPEX;
+    }
+
 }
